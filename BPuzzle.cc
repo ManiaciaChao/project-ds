ae562a8d472fc1d8d3add88bec5d2df064c5c685
maniacata <maniaciachao@gmail.com>
2020-03-30 00:03:39 +0800

    :construction: (BPuzzle) add flatten mapping for constraint3

--- BPuzzle.cc
+++ BPuzzle.cc
@@ -24,6 +24,75 @@ std::vector<std::vector<int>> all_combinations(int N, int K) {
   } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
   return combinations;
 }
+/**
+  * s -> {1, 2}
+  * x -> [1, size]
+  * y -> [1, size]
+  * u -> [1, size]
+  * b -> {0, 1}
+  * (x,y) -> [1, size^2]
+  * (s,x,y) -> size^2 + [1, 2 * C(size, 2)]
+  * (s,x,y,u) -> size^2 + 2 * C(size, 2) + [1, 2 * size * C(size, 2)]
+  * (s,x,y,u,b) -> size^2 + 2 * C(size, 2) + 2 * size * C(size, 2) + [1, 4 * size * C(size, 2)]
+  * */
+
+std::vector<std::vector<int>> n2_offset_table(int size) {
+  auto combinations = all_combinations(size, 2);
+  auto table = std::vector<std::vector<int>>{};
+  table.resize(size + 1);
+  table.clear();
+  for (auto &sub:table) {
+    sub.resize(size + 1);
+    sub.clear();
+  }
+  int i = 0;
+  for (const auto &combination:combinations) {
+    auto x = combination[0];
+    auto y = combination[1];
+    table[x][y] = i;
+    table[y][x] = i;
+    i++;
+  }
+  return table;
+}
+
+BPuzzle::BPuzzle() {
+  cnf = CNF{};
+  auto combinations = all_combinations(size, 2);
+  table = std::vector<std::vector<int>>{};
+  table.resize(size + 1);
+  table.clear();
+  for (auto &sub:table) {
+    sub.resize(size + 1);
+    sub.clear();
+  }
+  int i = 0;
+  for (const auto &combination:combinations) {
+    auto x = combination[0];
+    auto y = combination[1];
+    table[x][y] = i;
+    table[y][x] = i;
+    i++;
+  }
+  offset_sxy = size * size;
+  offset_sxyu = offset_sxy + 2*table.size();
+  offset_sxyub = offset_sxyu + table.size() * 2 * size;
+}
+
+// (s,x,y) -> size^2 + [1, 2 * C(size, 2)]
+int BPuzzle::flatten(int s, int x, int y) {
+  return offset_sxy + 2 * table[x][y] + s - 1;
+}
+// (s,x,y,u) -> size^2 + 2 * C(size, 2) + [1, 2 * size * C(size, 2)]
+int BPuzzle::flatten(int s, int x, int y, int u) {
+  auto n2_size = all_combinations(size, 2).size();
+  return offset_sxyu + table[x][y] * 2 * size + 2 * (u - 1) + s - 1;
+}
+// (s,x,y,u,b) -> size^2 + 2 * C(size, 2) + 2 * size * C(size, 2) + [1, 4 * size * C(size, 2)]
+int BPuzzle::flatten(int s, int x, int y,int u,int b) {
+  auto n2_size = all_combinations(size, 2).size();
+  return offset_sxyub + table[x][y] * 4 * size + 4 * (u-1) + 2 * (s-1) + b;
+}
 
 void BPuzzle::generate_cnf(std::string str) {
 //  cnf.literals
@@ -53,7 +122,7 @@ void BPuzzle::constraint2() {
   for (auto polar:polars) {
     for (int i = 1; i <= size; i++) {
       auto combinations = all_combinations(size, size / 2 + 1);
-      for (const auto& combination:combinations) {
+      for (const auto &combination:combinations) {
         for (auto var:combination) {
           Clause r_clause, c_clause;
           r_clause.push_back(Literal{flatten(i, var), polar});
@@ -70,6 +139,7 @@ void BPuzzle::constraint2() {
 
 void BPuzzle::constraint3() {
   auto id_start = size * size;
-  // 15711
+
 
 }
+
--- BPuzzle.h
+++ BPuzzle.h
@@ -13,14 +13,33 @@ class BPuzzle {
  private:
   int size = 4;
   CNF cnf;
-  Board board {};
+  std::vector<std::vector<int>> table;
+  int offset_sxy = 0;
+  int offset_sxyu = 0;
+  int offset_sxyub = 0;
+  Board board{};
  private:
-  inline int flatten(int i, int j) { return (i - i) * size + j - 1; };
+/**
+  * s -> {1, 2}
+  * x -> [1, size]
+  * y -> [1, size]
+  * u -> [1, size]
+  * b -> {0, 1}
+  * (x,y) -> [1, size^2]
+  * (s,x,y) -> size^2 + [1, 2 * C(size, 2)]
+  * (s,x,y,u) -> size^2 + 2 * C(size, 2) + [1, 2 * size * C(size, 2)]
+  * (s,x,y,u,b) -> size^2 + 2 * C(size, 2) + 2 * size * C(size, 2) + [1, 4 * size * C(size, 2)]
+  * */
+  inline int flatten(int x, int y) { return (x - 1) * size + y - 1; };
+  int flatten(int s, int x, int y);
+  int flatten(int s, int x, int y, int u);
+  int flatten(int s, int x, int y, int u, int b);
+
   void constraint1(); // no 3 consecutive same values for each row/column
   void constraint2(); // number of 1 or 0 should be same for each row/column
   void constraint3(); // no repeated row/column
  public:
-  BPuzzle() : cnf(CNF{}) {}
+  BPuzzle();
   explicit BPuzzle(CNF &cnf) : cnf(cnf) {}
   void init();
   void generate_cnf(std::string str);
